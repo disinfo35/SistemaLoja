92c53547f88e42472a96b8708a9bc6c295ff880d
Diego Melo <di.sinfo35@gmail.com>
2022-08-25 19:19:45 -0300

    Criei o controller de produto utilizando o Entity Framework

--- SistemaLoja/Models/Produto.cs
+++ SistemaLoja/Models/Produto.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaLoja.Models
 {
     public class Produto
     {
+        [Key]
+        public int ProdutoId { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime UltimaCompra { get; set; }
+        public float Estoque { get; set; }
+        public string Comentario { get; set; }
     }
 }
