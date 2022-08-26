167192db8ec3574f9477a97ffe4f6c690dcdf640
Diego Melo <di.sinfo35@gmail.com>
2022-08-25 22:11:04 -0300

    Dando o comando para migração automática e programando a migração

---
+++ SistemaLoja/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SistemaLoja.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SistemaLoja.Data.SistemaLojaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "SistemaLoja.Data.SistemaLojaContext";
+        }
+
+        protected override void Seed(SistemaLoja.Data.SistemaLojaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
